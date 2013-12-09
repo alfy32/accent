0bbdbcc591b56c78266ebbc2100c7f880a089620
Alan Christensen <alfy32@gmail.com>
2013-12-09 09:33:50 -0700

    Get westhost appointments by employee and start end date

--- routes/westhost.js
+++ routes/westhost.js
@@ -2,10 +2,30 @@ var request = require('request');
 
 module.exports = function (app) {
   app.get('/westhost-appointments', getWesthostAppointments);
+  app.get('/westhost-appointments/:employee', getWesthostAppointments);
 };
 
 function getWesthostAppointments(req, res) {
-  request('http://accent.westhostsite.com/data/json.php', function(err, resp, data) {
+  var start = req.query.start;
+  var end = req.query.end;
+
+  var employee = req.params.employee;  
+
+  var url = 'http://accent.westhostsite.com/data/json.php';
+
+  if(!start || !end) {
+    return res.send({
+      success: false,
+      err: 'Start and end date are required.'
+    });
+  }
+
+  url += '?start='+start+'&end='+end;
+
+  if(employee)
+    url += '&employee=' + employee;
+
+  request(url, function(err, resp, data) {
     if(err) {
       return res.send({
         success: false,
@@ -13,9 +33,6 @@ function getWesthostAppointments(req, res) {
       });
     }
 
-    res.send({
-      success: true,
-      appointments: JSON.parse(data)
-    });
+    res.send(JSON.parse(data));
   });
 };
