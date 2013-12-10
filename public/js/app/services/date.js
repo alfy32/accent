0c40eb9eee9b782e8a8304b13b4e590b1a79762a
Alan Christensen <alfy32@gmail.com>
2013-12-09 20:41:49 -0700

    updated the date module

--- public/js/app/services/date.js
+++ public/js/app/services/date.js
@@ -1,91 +1,107 @@
-angular.module('accent').factory('date', function (nav) {
-  	
-  	var pageDate = {
-  		value: new Date()
-  	};
+angular.module('accent').factory('date', 
+  function (nav) {
 
-    getDate();
-
-  	function getDate() {
-		var date = new Date();
-		var day = dayToNumber(JSDateToDay(date));
-
-		var dayStr = window.location.hash.split('/')[1];
-
-		if(dayStr) {
-			var offset = dayToNumber(dayStr) - day;	
-			date.setDate(date.getDate() + offset);
-		}
-
-		pageDate.value = date;
-		clearActive(nav);
-		var index = dayToNumber(JSDateToDay(pageDate.value));
-		nav[index].active = "active";
-	}	
-
-	
-
-    return {
-    	pageDate: pageDate,
-    	updatePageDate: getDate
+    var date = {
+      currentJSON: currentJSON,
+      dayToNumber: dayToNumber,
+      numberToDay: numberToDay,
+      jsDateToDay: jsDateToDay,
+      dateToJson: dateToJson,
+      jsonToDate: jsonToDate,
+      clearTime: clearTime,
+      getDate: getDate
     };
-});
 
-function clearActive(nav) {
-	for(var key in nav) {
-		nav[key].active = '';
-	}
-}
+    date.current = clearTime(new Date());
 
-function dayToNumber(day) {
-	switch(day) {
-	case "monday":
-		return 1;
-		break;
-	case "tuesday":
-		return 2;
-		break;
-	case "wednesday":
-		return 3;
-		break;
-	case "thursday":
-		return 4;
-		break;
-	case "friday":
-		return 5;
-		break;
-	case "saturday":
-		return 6;
-		break;
-	case "sunday":
-		return 7;
-		break;
-	}
-}
+    date.firstDayOfWeek = 'Sunday';
 
-function JSDateToDay(date) {
-	var number = date.getDay();
-	switch(number) {
-	case 1:
-		return "monday";
-		break;
-	case 2:
-		return "tuesday";
-		break;
-	case 3:
-		return "wednesday";
-		break;
-	case 4:
-		return "thursday";
-		break;
-	case 5:
-		return "friday";
-		break;
-	case 6:
-		return "saturday";
-		break;
-	case 0:
-		return "sunday";
-		break;
-	}
+    date.lastWeek = function() {
+      date.current.setDate(date.current.getDate() - 7);
+    }
+
+    date.nextWeek = function() {
+      date.current.setDate(date.current.getDate() + 7);
+    }
+
+    date.days = [
+      'sunday', 'monday','tuesday', 'wednesday', 
+      'thursday', 'friday', 'saturday'
+    ];
+
+    function currentJSON() {
+      return dateToJson(date.current);
+    }
+
+    function dayToNumber(day) { 
+      return date.days.indexOf(day.toLowerCase()) 
+    }
+    
+    function numberToDay(number) {
+      return date.days[number];
+    }
+
+    function jsDateToDay(iDate) {
+      return date.days[iDate.getDay()];
+    }
+
+    function dateToJson(iDate) {
+      return iDate.getFullYear() + 
+             '-' + pad2Digits(iDate.getMonth()+1) + 
+             '-' + pad2Digits(iDate.getDate());
+    }
+
+    function jsonToDate(dateString) {
+      if(dateString.match(/\d{4}-\d{2}-\d{2}/)) {
+
+        var splitDate = dateString.split('-');
+        var newDate = new Date();
+
+        newDate.setFullYear(splitDate[0]);
+        newDate.setMonth(+splitDate[1] - 1);
+        newDate.setDate(splitDate[2]);
+
+        date.clearTime(newDate);
+
+        return newDate;
+
+      } else {
+        return "Invalid date format";
+      }
+    }
+
+    function clearTime(iDate) {
+      iDate.setHours(0);
+      iDate.setMinutes(0);
+      iDate.setSeconds(0);
+      return iDate;
+    }
+
+    function getDate(theDate, dayOfWeek) {
+      var newDate = clearTime(new Date(theDate));
+      var dayNumber = dayToNumber(dayOfWeek);
+
+      var sunday = newDate.getDate() - newDate.getDay();
+
+      if(dayToNumber(dayOfWeek) < dayToNumber(date.firstDayOfWeek))
+        sunday += 7;
+
+      if(newDate.getDay() < dayToNumber(date.firstDayOfWeek))
+        sunday -= 7;
+
+      newDate.setDate(sunday + dayNumber);
+
+      return newDate;
+    }
+
+    return date;
+  }
+);
+
+function pad2Digits(value) {
+  if(+value < 10) {
+    return '0' + value;
+  }
+
+  return '' + value;
 }
