4db055dcf379cf248e9134ff0dac261088b86340
Alan Christensen <alfy32@gmail.com>
2013-12-09 21:28:37 -0700

    the report now shows every week in the year as a table.

--- public/js/app/services/report.js
+++ public/js/app/services/report.js
@@ -1,16 +1,38 @@
 angular.module('accent').factory('report',
-  function ($http) {
+  function ($http, date) {
     
     var report = {};
 
-    $http.get('/westhost-appointments').then(function(resp) {
-      if(resp.data.success) {
-        report.appointments = resp.data.appointments;
-        console.log(resp.data)
-      } else {
-        console.log('Failed to get westhost appointments.');
+    var jan1 = date.getFirstDayOfYear();
+    
+    var sunday = date.getDate(jan1, 'sunday');
+    var nextSunday = date.addDays(new Date(sunday), 7);
+
+    while(sunday.getFullYear() < 2014) {
+      getApointments(date.dateToJson(sunday), date.dateToJson(nextSunday), 'Jill');
+
+      date.addDays(sunday, 7);
+      date.addDays(nextSunday, 7);
+    }
+
+    function getApointments(start, end, employee) {
+      var url = '/westhost-appointments';
+
+      if(employee) {
+        url += '/'+employee;
       }
-    });
+
+      $http.get(url + '?start=' + start + '&end=' + end).then(function(resp) {
+        if(resp.data.success) {
+          report[start] = resp.data.appointments;
+          console.log(start, resp.data.appointments)
+        } else {
+          console.log('Failed to get westhost appointments.', resp.data.err);
+        }
+      });
+        
+    }
+    
 
     return report;
   }
