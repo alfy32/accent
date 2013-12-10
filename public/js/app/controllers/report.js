8d91a5f05d1a06b00c8db762681940a991c51304
Alan Christensen <alfy32@gmail.com>
2013-12-10 16:55:59 -0700

    working now

--- public/js/app/controllers/report.js
+++ public/js/app/controllers/report.js
@@ -6,5 +6,9 @@ angular.module('accent').controller('reportCtrl',
     $scope.report = report;
 
     $scope.date = date;
+
+    $scope.start = '2013-12-01';
+    $scope.end = '2013-12-31';
+    $scope.employee = 'Jill';
   }
 );
--- public/js/app/services/report.js
+++ public/js/app/services/report.js
@@ -1,24 +1,28 @@
 angular.module('accent').factory('report',
   function ($http, date) {
     
-    var report = {};
+    var report = {
+      appointments: {}
+    };
 
-    var jan1 = date.getFirstDayOfYear();
+    // var jan1 = date.getFirstDayOfYear();
 
-    jan1.setMonth(11);
+    // jan1.setMonth(11);
     // jan1.setDate(8);
 
-    var sunday = date.getDate(jan1, 'sunday');
-    var nextSunday = date.addDays(new Date(sunday), 7);
+    // var sunday = date.getDate(jan1, 'sunday');
+    // var nextSunday = date.addDays(new Date(sunday), 7);
 
-    while(sunday.getFullYear() < 2014) {
-      getApointments(date.dateToJson(sunday), date.dateToJson(nextSunday), 'Jill');
+    // while(sunday.getFullYear() < 2014) {
+    //   getApointments(date.dateToJson(sunday), date.dateToJson(nextSunday), employee);
 
-      date.addDays(sunday, 7);
-      date.addDays(nextSunday, 7);
-    }
+    //   date.addDays(sunday, 7);
+    //   date.addDays(nextSunday, 7);
+    // }
 
-    function getApointments(start, end, employee) {
+    report.getApointments = function(start, end, employee) {
+      report.appointments = {};
+
       var url = '/westhost-appointments';
 
       if(employee) {
@@ -27,11 +31,14 @@ angular.module('accent').factory('report',
 
       $http.get(url + '?start=' + start + '&end=' + end).then(function(resp) {
         if(resp.data.success) {
-          if(!report[start])
-            report[start] = {};
           
           for(var i in resp.data.appointments) {
             var appointment = resp.data.appointments[i];
+            start = date.getDate(appointment.date, 'sunday');
+            start = date.dateToJson(start);
+            
+            if(!report.appointments[start])
+              report.appointments[start] = {};
 
             var time = normalizeTime(appointment.startTime);
             var dayOfWeek = date.jsonToDate(appointment.date).getDay();
@@ -40,30 +47,76 @@ angular.module('accent').factory('report',
 
             var client = '';
             
-            var MAX_LEN = 20;
+            var MAX_LEN = 150;
             var i = 0;
 
-            for(; i < clientStrings.length && client.length + clientStrings[i].length < MAX_LEN; i++) {
+            for(; i < clientStrings.length && getTextWidth(client + ' ' + clientStrings[i]) < MAX_LEN; i++) {
               client += ' ' + clientStrings[i];
+
+              client = client.trim();
             }
 
+            var leftOverIndex = -1;
+            var leftOver = [];
 
+            for(; i < clientStrings.length; i++) {
+              if(leftOverIndex < 0 || getTextWidth(leftOver[leftOverIndex] + ' ' + clientStrings[i]) > MAX_LEN)
+                leftOver[++leftOverIndex] = '';
 
-            // if(client.length >= MAX_LEN) {
-            //   client = client.substr(0,MAX_LEN);
-            // }
+              leftOver[leftOverIndex] += ' ' + clientStrings[i];
 
-            if(!report[start][date.days[dayOfWeek]])
-              report[start][date.days[dayOfWeek]] = {}
+              leftOver[leftOverIndex] = leftOver[leftOverIndex].trim();
+            }
 
-            report[start][date.days[dayOfWeek]][time] = {
+            if(!report.appointments[start][date.days[dayOfWeek]])
+              report.appointments[start][date.days[dayOfWeek]] = {}
+
+            report.appointments[start][date.days[dayOfWeek]][time] = {
+              appointment: appointment,
               date: appointment.date,
               time: time,
               blocks: appointment.endTime,
               client: client,
               employee: appointment.employee,
-              clientFull: appointment.client
+              title: appointment
             };
+
+            if(leftOver.length) {
+              var splitTime = time.split(':');
+              var min = 15 + time[1];
+              if(time)
+              var time = time
+            }
+
+            for(var i = 0; i < leftOver.length; i++) {
+              time = add15min(time);
+              report.appointments[start][date.days[dayOfWeek]][time] = {
+                appointment: appointment,
+                date: appointment.date,
+                time: time,
+                blocks: appointment.endTime,
+                client: leftOver[i],
+                employee: appointment.employee,
+                title: appointment
+              };
+            }
+
+            time = add15min(time);
+
+            while(unnormalizeTime(time) < appointment.endTime && 
+                  !report.appointments[start][date.days[dayOfWeek]][time]) {
+              console.log(unnormalizeTime(time), appointment.endTime)
+              report.appointments[start][date.days[dayOfWeek]][time] = {
+                appointment: appointment,
+                date: appointment.date,
+                time: time,
+                blocks: appointment.endTime,
+                client: 'x',
+                employee: appointment.employee,
+                title: appointment
+              };
+              time = add15min(time);
+            }
           }
         } else {
           console.log('Failed to get westhost appointments.', resp.data.err);
@@ -81,6 +134,17 @@ angular.module('accent').factory('report',
       
       return hour + ':' + min;
     }
+
+    function unnormalizeTime(time) {
+      var time = time.split(':');
+      var hour = time[0];
+      var min = time[1];
+
+      if(hour < 7) hour = +hour+12
+      if(hour < 10) hour = '0' + +hour;
+      
+      return hour + ':' + min + ':00';
+    }
     
 
     return report;
@@ -89,4 +153,39 @@ angular.module('accent').factory('report',
 
 function jsonTimeToMin(time) {
   var splitTime = time.split(':');
+}
+
+function getTextWidth(text) {
+  var div = $('<div>');
+
+  div.css('display', 'inline-block');
+  div.hide();
+
+  $('body').append(div);
+
+  div.text(text);
+
+  var width = div.width();
+
+  div.remove();
+
+  return width;
+}
+
+function add15min(time) {
+  time = time.split(':');
+  var hr = time[0];
+  var min = time[1];
+
+  min = +min + 15;
+
+  if(min >= 60) {
+    min = '00';
+    hr = +hr + 1;
+  }
+
+  if(+hr > 12) hr = '01';
+  if(+hr < 10) hr = '0' + +hr;
+
+  return hr + ':' + min
 }
