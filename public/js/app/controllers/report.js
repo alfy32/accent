7fdd1b0c6149feee585f1d342657347388a7ea72
Alan Christensen <alfy32@gmail.com>
2013-12-10 17:03:46 -0700

    added all employees

--- public/js/app/controllers/report.js
+++ public/js/app/controllers/report.js
@@ -10,5 +10,11 @@ angular.module('accent').controller('reportCtrl',
     $scope.start = '2013-12-01';
     $scope.end = '2013-12-31';
     $scope.employee = 'Jill';
+
+    $scope.employees = [
+      "Jill","Mary","Marva","KerrieLynn",
+      "Sindy","Jen","Linda","Jeaneen",
+      "Kat","Kortney","Teri","Jackie"
+    ]; 
   }
 );
